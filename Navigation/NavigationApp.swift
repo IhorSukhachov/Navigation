066671210681a7dd317c954eaadf6c930e1ccb9b
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-11-24 15:40:12 +0200

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Ihor Sukhachov on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
